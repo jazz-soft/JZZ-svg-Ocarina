f75531602ade6d56a534c7486584f429e0c10de9
Sema <jazz-dev@jazz-soft.net>
2025-11-06 23:57:06 -0500

    Hole render

--- javascript/JZZ.gui.Ocarina.js
+++ javascript/JZZ.gui.Ocarina.js
@@ -15,48 +15,52 @@
   if (!JZZ.gui) JZZ.gui = {};
   if (JZZ.gui.Ocarina) return;
 
-  function Dot(x, y, r, t) {
+  function Hole(x, y, r, t) {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.state = 0;
   }
-  Dot.prototype.dump = function() {
-    return '<circle cx="' + this.x + '" cy="' + this.y + '" r="' + this.r + '"/>';
+  Hole.prototype.dump = function() {
+    return '<circle stroke-width="1px" vector-effect="non-scaling-stroke" stroke="currentColor" fill="' + (this.state ? 'currentColor' : 'none') + '" cx="' + this.x + '" cy="' + this.y + '" r="' + this.r + '"/>';
   };
-  Dot.prototype.render = function() {
-    if (!this.svg) {
-      var svg = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-      svg.setAttribute("cx", this.x);
-      svg.setAttribute("cy", this.y);
-      svg.setAttribute("r", this.r);
-      svg.setAttribute("stroke", "black");
-      svg.setAttribute("vector-effect", "non-scaling-stroke");
-      svg.setAttribute("stroke-width", "1px");
-      this.svg = svg;
-    }
-    return this.svg;
+  Hole.prototype.render = function(at) {
+    if (at) this.at = at;
+    if (this.svg) arguments.removeChild(this.svg);
+    this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    this.svg.setAttribute('cx', this.x);
+    this.svg.setAttribute('cy', this.y);
+    this.svg.setAttribute('r', this.r);
+    this.svg.setAttribute('stroke', 'currentColor');
+    this.svg.setAttribute('fill', this.state ? 'currentColor' : 'none');
+    this.svg.setAttribute('vector-effect', 'non-scaling-stroke');
+    this.svg.setAttribute('stroke-width', '1px');
+    at.appendChild(this.svg);
   };
-  const _dots = [[0, 0, 0.1], [0.5, 0.5, 0.03], [1, 1, 0.05]];
+  Hole.prototype.set = function(x) {
+    if (this.state == x) return;
+    this.state = x;
+    this.render();
+  }
 
   function Ocarina(arg) {
     var self = new JZZ.Widget();
     var i, d, svg;
     if (!arg) arg = {};
-    if (!arg.dots) arg.dots = _dots;
-    self.dots = [];
-    for (i = 0; i < arg.dots.length; i++) {
-      d = arg.dots[i];
-      self.dots.push(new Dot(d[0], d[1], d[2]));
+    self.holes = [];
+    for (i = 0; i < arg.holes.length; i++) {
+      d = arg.holes[i];
+      self.holes.push(new Hole(d[0], d[1], d[2]));
     }
     self.key = JZZ.MIDI.noteValue(arg.key);
     if (self.key == undefined) self.key = 60;
     self.back = arg.back;
     if (arg.at) {
       try {
-        svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("version", "1.1");
-        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        svg.setAttribute("viewBox", "0 0 1 1");
+        svg = document.createElementNS('http://www.w3.org/2000/svg', "svg");
+        svg.setAttribute('version', "1.1");
+        svg.setAttribute('xmlns', "http://www.w3.org/2000/svg");
+        svg.setAttribute('viewBox', "0 0 1 1");
       }
       catch (e) {
         svg = undefined;
@@ -72,29 +76,28 @@
         }
         self.svg = svg;
         if (self.back) self.svg.innerHTML = self.back;
-        for (i = 0; i < self.dots.length; i++) {
-          d = self.dots[i].render();
-          if (d) svg.appendChild(d);
-        }
+        for (i = 0; i < self.holes.length; i++) self.holes[i].render(svg);
       }
     }
     self.chan = arg.chan || 0;
     self._receive = function(msg) {
-      _emit(msg);
+      self._emit(msg);
     }
+    self.dump = function(w, h) {
+      var svg = [];
+      if (this.back) svg.push(this.back);
+      svg.push('<g>');
+      for (i = 0; i < this.holes.length; i++) svg.push(this.holes[i].dump());
+      svg.push('</g>');
+      return svg.join('\n');
+    };
+    self.set = function(n, a) {
+    };
+    self.reset = function() {
+      for (var i = 0; i < this.holes.length; i++) this.holes[i].set(0);
+    };
     return self;
   }
-  Ocarina.prototype.set = function(n, a) {
-  }
-  Ocarina.prototype.reset = function() {
-    for (var i = 0; i < this.dots.length; i++) this.dots[i].reset();
-  }
-  Ocarina.prototype.dump = function(w, h) {
-    var svg = [];
-    if (this.back) svg.push(this.back);
-    for (i = 0; i < this.dots.length; i++) svg.push(this.dots[i].dump());
-    return svg.join('\n');
-  };
 
   JZZ.gui.Ocarina = Ocarina;
 });
